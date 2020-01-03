d81257944aca2546088943870c6b158c79a2dd07
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-03 17:54:33 -0500

    add textinput and event listener

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('hello');
+// http://www.omdbapi.com/?apikey=[yourkey]& - data requests
+// http://img.omdbapi.com/?apikey=[yourkey]& - poster requests
+
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '5b7ffe64',
+            s: searchTerm
+        }
+    });
+    console.log(response.data);
+}
+
+const input = document.querySelector('input');
+let timeoutID;
+
+const onInput = event => {
+    if (timeoutID)
+        clearTimeout(timeoutID);
+
+    timeoutID = setTimeout( ()=> {
+        fetchData(event.target.value);
+    }, 500);
+}
+
+input.addEventListener('input', onInput);
